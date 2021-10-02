c9ba2ee9de6b5d093ca8bbb1d09bf645561fe207
Wembley Leach <wembley@hey.com>
2021-10-02 16:27:57 -0400

    Remove debug banner and setup app for working

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
+          bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
       home: WelcomeScreen(),
